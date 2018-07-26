1ff04c4c1afb745a10aff3dbffd9fbfd497c89f3
kornholio1975 <gregory.fonkatz@gmail.com>
2018-07-25 20:53:06 -0400

    Cosmit + docstrings

--- crdt_main.py
+++ crdt_main.py
@@ -1,20 +1,33 @@
 import collections
 
-class LWWElement(dict, collections.MutableMapping):
+class LWWEInset(dict, collections.MutableMapping):
+    """Dict subclass for storing LWW Elements in CRDT LWWElementSet.
+
+    Stores a key value pair where::
+        key: any serializble type storing a payload
+        value: epoch time expressed as 32bit integer representing a timestamp
+    """
     def __setitem__(self, payload, timestamp):
+        #Override dict __setitem__ to enforce correct data types
         if not isinstance(payload, collections.Hashable):
             raise TypeError('unhashable type {}'.format(type(payload)))
         if not isinstance(timestamp, int):
             raise TypeError('timestamp expected to be an epoch time expressed in 32 bit integer')
-        super(LWWElement, self).__setitem__(payload, timestamp)
+        super(LWWEInset, self).__setitem__(payload, timestamp)
 
 class LWWElementSet(object):
-    """Conflict Free Replicated Data Type (CRDT) implemented 
-        using Last Writer Wins (LWW) algorythm.
+    """Conflict Free Replicated Data Type (CRDT) implemented using Last Writer Wins (LWW) algorythm.
+
+    Usage::
+        >>> import crdt_main
+        >>> data_set = crdt_main.LWWElementSet()
+        >>> data_set.add('foo', 1532565895)
+        >>> data_set.remove('foo', 1532565941)
     """
+
     def __init__(self):
-        self._set_add = LWWElement()
-        self._set_remove = LWWElement()
+        self._set_add = LWWEInset()
+        self._set_remove = LWWEInset()
 
     def add(self, payload, timestamp):
         self._set_add[payload] = timestamp
